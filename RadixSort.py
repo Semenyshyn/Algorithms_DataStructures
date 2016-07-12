7004a3f96f273bf111d9b2c734ae7a985008da36
Semenyshyn <simen.sims@gmail.com>
2016-07-12 17:51:02 +0300

    1.1.3.

--- RadixSort.py
+++ RadixSort.py
@@ -1,7 +1,6 @@
 import urllib.request
-from itertools import product
 from collections import Counter
-from string import ascii_lowercase
+
 
 a = urllib.request.urlopen("http://courses.prometheus.org.ua/c4x/KPI/Algorithms101/asset/anagrams.txt").read()
 a = a.decode("utf-8")
@@ -11,18 +10,11 @@ n = Counter(a)
 
 def counting_sort(l, d):
     letters = [chr(i) for i in range(97, 123)]
-    keywords = [''.join(i) for i in product(ascii_lowercase, repeat=3)]
-    items = dict()
-    for keyword in keywords:
-        items[keyword] = 0
-    for item in l:
-        items[item] += 1
     sort_l = list()
     for letter in letters:
         for keyword in l:
             if letter == keyword[d]:
                 sort_l.append(keyword)
-                items[keyword] -= 1
     return sort_l
 
 
@@ -34,4 +26,4 @@ def radix_sort(l, d):
 
 w = radix_sort(b, 2)
 print(w)
-print(sorted(b))
+
