5bc73ef9f40a6c9396872d2d949305fc4aafcfb8
Semenyshyn <simen.sims@gmail.com>
2016-07-12 17:43:22 +0300

    1.1.3.

--- RadixSort.py
+++ RadixSort.py
@@ -1,14 +1,17 @@
 import urllib.request
 from itertools import product
+from collections import Counter
 from string import ascii_lowercase
 
 a = urllib.request.urlopen("http://courses.prometheus.org.ua/c4x/KPI/Algorithms101/asset/anagrams.txt").read()
 a = a.decode("utf-8")
 b = a.split()
+n = Counter(a)
+
 
 
 def counting_sort(l, d):
-    letters = [chr(i) for i in range(97, 122)]
+    letters = [chr(i) for i in range(97, 123)]
     keywords = [''.join(i) for i in product(ascii_lowercase, repeat=3)]
     items = dict()
     for keyword in keywords:
@@ -19,13 +22,19 @@ def counting_sort(l, d):
     for letter in letters:
         for keyword in l:
             if letter == keyword[d]:
-                sort_l.extend(keyword * items[keyword])
-    sorted_list = [i+j+z for i, j, z in zip(sort_l[::3], sort_l[1::3], sort_l[2::3])]
-    return sorted_list
+                sort_l.append(keyword)
+                items[keyword] -= 1
+    return sort_l
 
 
-o = ['abc', 'dcb', 'abb']
-p = counting_sort(b, 2)
-print(counting_sort(p, 1))
+def radix_sort(l, d):
+    for i in range(d):
+        l = counting_sort(l, i)
+    return l
 
-
+o = ['ahk', 'ahz', 'aib']
+x = counting_sort(b, 2)
+y = counting_sort(x, 1)
+z = counting_sort(y, 0)
+print(z)
+print(sorted(b))
